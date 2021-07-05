064be1835bacfd42c6641fcc92e0b0f9f8999096
TheoChauvreau <theo.chauvreau@viacesi.fr>
2021-07-05 11:29:23 +0100

    test d'un merge chelou

--- quizz-lib.js
+++ quizz-lib.js
@@ -1,56 +1,96 @@
-var JSON_FILE = './test-files/test1.json';
-var DISPLAY_HINTS = true; //true : hints are displayed //false : hints aren't displayed
-var MAIN_TITLE_TYPE = "h2"; //h1 to h6
-var QUESTION_TITLE_TYPE = "h3"; //h1 to h6
-var NORMAL_TEXT_TYPE = "p";
+var config = {
+    JSON_FILE: './test-files/test1.json',
+    DISPLAY_HINTS: true, //true : hints are displayed //false : hints aren't displayed
+    MAIN_TITLE_TYPE: "h1", //h1 to h6
+    QUESTION_TITLE_TYPE: "h2", //h1 to h6
+    NORMAL_TEXT_TYPE: "p",
+    lang: {
+        button: {
+            validate: "Valider",
+            displayHint: "Afficher l'indice {0}"
+        }
+    },
+    types: {
+        QUESTION_QCM: "QCM", 
+        QUESTION_OPEN: "OPEN", 
+        QUESTION_LINKED: "LINKED"    
+    },
+    elements: {
+        ELEMENT_BUTTON: "button",
+        ELEMENT_INPUT: "input",
+        ELEMENT_CHECKBOX: "checkbox"
+    },
+    ids: {
+        ID_MAIN_DIV: "quizz-app", 
+        ID_MAIN_TITLE: "quizz-app-name", 
+        ID_MAIN_DESCRIPTION: "quizz-app-description", 
+        ID_MAIN_QUESTIONS: "quizz-app-questions",
+        ID_MAIN_VALIDATE: "quizz-app-validate",
+        questions: {
+            ID_QUESTION_NAME: "quizz-app-questions-{0}-name",
+            ID_QUESTION_ANSWERS: "quizz-app-questions-{0}-answers",
+            ID_QUESTION_ANSWER: "quizz-app-questions-{0}-answers-{1}",
+            ID_QUESTION_CHECKBOX: "quizz-app-questions-{0}-answers-{1}-checkbox",
+            ID_QUESTION_TEXT: "quizz-app-questions-{0}-answers-{1}-text",
+            ID_QUESTION_INPUT: "quizz-app-questions-{0}-input"
+
+        }
+    },
+    class: {
+
+    }
+};
 
 var loader = setInterval(function () {
     if(document.readyState !== "complete") return;
     clearInterval(loader);
-    getJSON();
+    createNewQuizz(json);
 }, 300);
 
 function createNewQuizz(jsonObject) {
-    var dom = document.getElementById("quizz-app");
+    if(jsonObject.display_hints != config.DISPLAY_HINTS)
+        config.DISPLAY_HINTS = jsonObject.display_hints;
+
+    var dom = document.getElementById(config.ids.ID_MAIN_DIV);
     console.log(dom);
 
-	displayTitleNode(jsonObject.name, MAIN_TITLE_TYPE, "quizz-app-name", dom);
+	displayTitleNode(jsonObject.name, config.MAIN_TITLE_TYPE, config.ids.ID_MAIN_TITLE, dom);
 
-	displayTextNode(jsonObject.description, "quizz-app-description", dom);
+	displayTextNode(jsonObject.description, config.ids.ID_MAIN_DESCRIPTION, dom);
 
     var quizzQuestions = jsonObject.questions;
-    var divQuestions = createDivNode("quizz-app-questions", "questions", dom);
+    var divQuestions = createDivNode(config.ids.ID_MAIN_QUESTIONS, dom);
 
     quizzQuestions.forEach(function(question, index) {
-        if(question.type == "QCM")
+        if(question.type == config.types.QUESTION_QCM)
             createNewQCM(question, divQuestions, index);
-        else if(question.type == "OPEN")
+        else if(question.type == config.types.QUESTION_OPEN)
             createNewOpen(question, divQuestions, index);
-        else if(question.type == "LINKED")
+        else if(question.type == config.types.QUESTION_LINKED)
             createNewLinked(question, divQuestions, index);
     });
 
-	//button
-    var buttonValidate = document.createElement("button");
-    buttonValidate.innerHTML="valider";
-    divQuestions.appendChild(buttonValidate);
+    createButton(config.lang.button.validate, config.ids.ID_MAIN_VALIDATE, dom, function() { validateQuizz(quizzQuestions); });
 }
 
 function createNewQCM(question, dom, questionId) {
-	displayTitleNode(question.name, QUESTION_TITLE_TYPE, "quizz-app-questions-" + questionId + "-name", dom);
+	displayTitleNode(question.name, config.QUESTION_TITLE_TYPE, config.ids.questions.ID_QUESTION_NAME.format(questionId), dom);
+
+	if(config.DISPLAY_HINTS)
+        displayHints(question.hints, questionId, dom);
 
     var questionAnswers = question.answers;
-    var divAnswers = createDivNode("quizz-app-questions-" + questionId + "-answers", "answers", dom);
+    var divAnswers = createDivNode(config.ids.questions.ID_QUESTION_ANSWERS.format(questionId), dom);
 
     questionAnswers.forEach(function(answer, index) {
-        var divAnswer = createDivNode("quizz-app-questions-" + questionId + "-answers-" + index, "answer", divAnswers);
+        var divAnswer = createDivNode(config.ids.questions.ID_QUESTION_ANSWER.format(questionId, index), divAnswers);
         
-        var checkboxAnswer = document.createElement("input");
-        checkboxAnswer.type = "checkbox";
-        checkboxAnswer.id = "quizz-app-questions-" + questionId + "-answers-" + index + "-checkbox";
+        var checkboxAnswer = document.createElement(config.elements.ELEMENT_INPUT);
+        checkboxAnswer.type = config.elements.ELEMENT_CHECKBOX;
+        checkboxAnswer.id = config.ids.questions.ID_QUESTION_CHECKBOX.format(questionId, index);
         divAnswer.appendChild(checkboxAnswer);
 
-		displayTextNode(answer, "quizz-app-questions-" + questionId + "-answers-" + index + "-text", divAnswer);
+		displayTextNode(answer, config.ids.questions.ID_QUESTION_TEXT.format(questionId, index), divAnswer);
     });
 
     if(DISPLAY_HINTS)
@@ -58,45 +98,33 @@ function createNewQCM(question, dom, questionId) {
 }
 
 function createNewOpen(question, dom, questionId) {
-	displayTitleNode(question.name, QUESTION_TITLE_TYPE, "quizz-app-questions-" + questionId + "-name", dom);
+	displayTitleNode(question.name, config.QUESTION_TITLE_TYPE, config.ids.questions.ID_QUESTION_NAME.format(questionId), dom);
 
-    var inputSolution = document.createElement("input");
+    if(config.DISPLAY_HINTS)
+        displayHints(question.hints, questionId, dom);
+
+    var inputSolution = document.createElement(config.elements.ELEMENT_INPUT);
     inputSolution.type = "text";
     inputSolution.required = true;
-    dom.appendChild(inputSolution);
-
-    if(DISPLAY_HINTS)
-        displayHints(question.hints, questionId, dom);
+    inputSolution.id = config.ids.questions.ID_QUESTION_INPUT.format(questionId);
+    dom.appendChild(inputSolution); 
 }
 
 function createNewLinked(question, dom, questionId) {
-    displayTitleNode(question.name, QUESTION_TITLE_TYPE, "quizz-app-questions-" + questionId + "-name", dom);
+    displayTitleNode(question.name, config.QUESTION_TITLE_TYPE, config.ids.questions.ID_QUESTION_NAME.format(questionId), dom);
+
+    if(config.DISPLAY_HINTS)
+        displayHints(question.hints, questionId, dom);
 
     var questionAnswers = question.answers;
     var questionSolutions = shuffle(question.solution);
 	var divAnswersAndSolutions = createDivNode("quizz-app-questions-" + questionId + "-answersAndSolutions", "solutions", dom);
 
-    questionAnswers.forEach(function(answer, index) {
-		var divAnswer = createDivNode("quizz-app-questions-" + questionId + "-answers-" + index, "answers", divAnswersAndSolutions);
+    var ulAnswers = createUlNode("quizz-app-questions-" + questionId + "-answers", divAnswersAndSolutions, questionAnswers);
         
-        displayTextNode(answer, "quizz-app-questions-" + questionId + "-answers-" + index + "-text", divAnswer);
+    var ulSolutions = createUlNode("quizz-app-questions-" + questionId + "-solutions", divAnswersAndSolutions, questionSolutions);
 
-        var inputSolution = document.createElement("select");
-        inputSolution.id = "quizz-app-questions-" + questionId + "-answers-" + index + "-select";
-        divAnswer.appendChild(inputSolution);
-
-        for (var i = 1; i <= questionAnswers.length; i++) {
-            var option = document.createElement("option");
-            option.value = i;
-            option.text = i;
-            inputSolution.appendChild(option);
-        }
-
-		displayTextNode(questionSolutions[index], "quizz-app-questions-" + questionId + "-answers-" + index + "-textSolution", divAnswer);
-    });
-
-    if(DISPLAY_HINTS)
-        displayHints(question.hints, questionId, dom);
+    new Sorter(ulSolutions);
 }
 
 function displayTextNode(text, id, dom) {
@@ -121,9 +149,9 @@ function displayHints(hints, questionId, dom) {
 
 	hints.forEach(function(hint, index) {
         if(hint == "") return;
-		createButton("Afficher indice " + (index + 1), "quizz-app-questions-" + questionId + "hints-button-" + index, "hint", divHints, function() {
+		createButton(config.lang.button.displayHint.format(index + 1), "quizz-app-questions-" + questionId + "hints-button-" + index, divHints, function() {
 			var button = document.getElementById("quizz-app-questions-" + questionId + "hints-button-" + index);
-			displayTitleNode(hints[index], NORMAL_TEXT_TYPE, "quizz-app-questions-" + questionId + "hints-" + index, divHints);
+			displayTitleNode(hints[index], config.NORMAL_TEXT_TYPE, "quizz-app-questions-" + questionId + "hints-" + index, divHints);
 			button.remove();
 		});
 	});
@@ -146,16 +174,18 @@ function createDivNode(id, classe, dom) {
 	return divNode;
 }
 
-function getJSON() {
-    var xmlhttp = new XMLHttpRequest();
+function createUlNode(id, dom, liElements) {
+    var ul = document.createElement('ul');
+    ul.id = id;
+    dom.appendChild(ul);
 
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200)
-            createNewQuizz(JSON.parse(this.responseText));
-    };
-
-    xmlhttp.open("GET", JSON_FILE, true);
-    xmlhttp.send(); 
+    liElements.forEach(function(content, index) {
+        var li = document.createElement('li');
+        li.innerHTML += content;
+        li.id = id + "-" + index;
+        ul.appendChild(li);
+    });
+    return ul;
 }
 
 function shuffle(array) {
@@ -170,10 +200,141 @@ function shuffle(array) {
     return array;
 }
 
-/*
-function send(array){
-    var finalUserArray = [];
-    var SolutionArray = [];
-    
-} 
-*/
+function allowDrop(ev) { 
+    ev.dataTransfer.setData("text", ev.target.id); 
+}
+
+function drag(ev) { 
+    ev.preventDefault();
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+function validateQuizz(questions) {
+    var rightQuestions = 0;
+    questions.forEach(function(question, index) {
+        if(question.type == config.types.QUESTION_QCM) {
+            if(validateQCM(question, index))
+                rightQuestions++;
+        }
+        else if(question.type == config.types.QUESTION_OPEN) {
+            if(validateOpen(question, index))
+                rightQuestions++;
+        }
+        // else if(question.type == config.types.QUESTION_LINKED)
+        //     validateLinked(question, index);
+    });
+    console.log(rightQuestions);
+}
+
+function validateQCM(question, questionId) {
+    var solution = question.solution;
+    var answers = question.answers;
+    var rightAnswers = 0;
+    answers.forEach(function(answer, index) {
+        if(document.getElementById(config.ids.questions.ID_QUESTION_CHECKBOX.format(questionId, index)).checked)
+            if(solution.includes(index+1))
+                rightAnswers++;
+    });
+    return rightAnswers == solution.length;
+}
+
+function validateOpen(question, questionId) {
+    var solution = question.solution;
+    var elem = document.getElementById(config.ids.questions.ID_QUESTION_INPUT.format(questionId));
+    console.log(elem.value);
+    if(question.caseSensitive) {
+        if(elem.value == solution)
+            return true;
+    }
+    else {
+        if(elem.value.toLowerCase() == solution.toLowerCase())
+            return true;
+    }
+    return false;
+}
+
+String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+        return typeof args[number] != 'undefined' ? args[number] : match;
+    });
+};
+
+/**
+ * @author alsolovyev
+ * @license MIT
+ * https://www.cssscript.com/draggable-list-sorter/
+ */
+var Sorter = (function () {
+    "use strict";
+    function e() {
+        return (e =
+            Object.assign ||
+            function (e) {
+                for (var t = 1; t < arguments.length; t++) {
+                    var n = arguments[t];
+                    for (var i in n) Object.prototype.hasOwnProperty.call(n, i) && (e[i] = n[i]);
+                }
+                return e;
+            }).apply(this, arguments);
+    }
+    return (function () {
+        function t(e) {
+            if (!(e instanceof Element)) throw TypeError("Sorter: 'container' must be an HTMLElement, not " + {}.toString.call(e));
+            (this._container = e), (this._children = Array.from(this._container.children)), (this._isDragging = !1), (this._draggedItem = null), (this._draggedIndex = null), this._bind(), this._addListeners();
+        }
+        var n = t.prototype;
+        return (
+            (n._findChild = function (e) {
+                for (; e.parentNode !== this._container; ) e = e.parentNode;
+                return e;
+            }),
+            (n._insertChild = function (e) {
+                var t = this._findIndex(e);
+                this._draggedIndex !== t &&
+                    (this._container.insertBefore(this._draggedItem, this._draggedIndex < t ? e.nextSibling : e), this._children.splice(this._draggedIndex, 1), this._children.splice(t, 0, this._draggedItem), (this._draggedIndex = t));
+            }),
+            (n._findIndex = function (e) {
+                var t = this._children.indexOf(e);
+                if (t < 0) throw new Error("Element is not a child of the container");
+                return t;
+            }),
+            (n._toggleElementStyles = function (t, n) {
+                if (t.hasAttribute("style")) return t.removeAttribute("style");
+                if ("object" != typeof n) throw new Error("The 'styles' expects a mapping from style properties to values");
+                e(t.style, n);
+            }),
+            (n._onMouseDown = function (e) {
+                e.target !== this._container &&
+                    (e.preventDefault(),
+                    (this._isDragging = !0),
+                    (this._draggedItem = this._findChild(e.target)),
+                    (this._draggedIndex = this._findIndex(this._draggedItem)),
+                    this._toggleElementStyles(this._draggedItem, { opacity: "0.75" }),
+                    this._toggleElementStyles(this._container, { cursor: "move" }));
+            }),
+            (n._onMouseOver = function (e) {
+                var t = e.target;
+                this._isDragging && t !== this._container && this._insertChild(this._findChild(t));
+            }),
+            (n._onMouseUp = function () {
+                this._isDragging && (this._toggleElementStyles(this._draggedItem), this._toggleElementStyles(this._container), (this._isDragging = !1), (this._draggedItem = null), (this._draggedIndex = null));
+            }),
+            (n._addListeners = function () {
+                this._container.addEventListener("mousedown", this._onMouseDown), this._container.addEventListener("mouseover", this._onMouseOver), document.addEventListener("mouseup", this._onMouseUp);
+            }),
+            (n._bind = function () {
+                (this._onMouseDown = this._onMouseDown.bind(this)), (this._onMouseOver = this._onMouseOver.bind(this)), (this._onMouseUp = this._onMouseUp.bind(this));
+            }),
+            (n.destroy = function () {
+                this._container.removeEventListener("mousedown", this._onMouseDown), this._container.removeEventListener("mouseover", this._onMouseOver), document.removeEventListener("mouseup", this._onMouseUp);
+            }),
+            t
+        );
+    })();
+})();
---
+++ test-files/test1.js
@@ -0,0 +1,50 @@
+var json = {
+    name: "Quizz de test",
+    description: "ce quizz est fait pour les tests",
+    display_hints: true,
+    questions: [
+        {
+            type: "QCM",
+            name: "Combien font 2+3 ?",
+            hints: ["indice1","indice2"],
+            optionals: [
+                {
+                    type: "IMAGE",
+                    link: "",
+                    sizeX: 0,
+                    sizeY: 0
+                },
+                {
+                    type: "CODE",
+                    content: ""
+                },
+                {
+                    type: "VIDEO",
+                    link: ""
+                },
+                {
+                    type: "AUDIO",
+                    link: ""
+                }
+            ],
+            answers: ["1","7", "5"],
+            solution: [3]
+        },
+        {
+            type: "OPEN",
+            name: "Quel est le prénom de Mr.GUIBERT ?",
+            hints: ["commence par un O","Est le nom d'une plante"],
+            optionals: [],
+            solution: "Olivier",
+            caseSensitive: false
+        },
+        {
+            type: "LINKED",
+            name: "Lier chaque mot à son synonyme",
+            hints: ["",""],
+            optionals: [],
+            answers: ["oui", "non"],
+            solution: ["certainement", "bof"]
+        }
+    ]
+};
